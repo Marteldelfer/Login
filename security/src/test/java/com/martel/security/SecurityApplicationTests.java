6f65c26c6a17d5047a571d1df61d1fd6fff73802
Murillo Ferraz <marteldelfer@gmail.com>
2024-10-03 12:21:08 -0300

    Create and configure database

---
+++ security/src/test/java/com/martel/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martel.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
