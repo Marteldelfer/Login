669bab35b827256e03138d58121cd95e6b719f20
Murillo Ferraz <marteldelfer@gmail.com>
2024-10-09 22:15:29 -0300

    save and expire tokens

--- security/src/main/java/com/martel/security/auth/AuthenticationService.java
+++ security/src/main/java/com/martel/security/auth/AuthenticationService.java
@@ -6,6 +6,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.martel.security.config.JwtService;
+import com.martel.security.token.Token;
+import com.martel.security.token.TokenRepository;
+import com.martel.security.token.TokenType;
 import com.martel.security.user.AppUser;
 import com.martel.security.user.Role;
 import com.martel.security.user.UserRepository;
@@ -17,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthenticationService {
 
     private final UserRepository repository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
@@ -29,8 +33,12 @@ public class AuthenticationService {
             .password(passwordEncoder.encode(request.getPassword()))
             .role(Role.USER)
             .build();
-        repository.save(user);
+        AppUser savedUser = repository.save(user);
         var jwtToken = jwtService.generateToken(user);
+
+        revokeAllUserTokens(savedUser);
+        saveUserToken(savedUser, jwtToken);
+        
         return AuthenticationResponse.builder()
             .token(jwtToken)
             .build();
@@ -43,11 +51,37 @@ public class AuthenticationService {
              );
         var user = repository.findByEmail(request.getEmail())
             .orElseThrow();
-        var jwtToken = jwtService.generateToken(user);    
+        var jwtToken = jwtService.generateToken(user); 
+
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);   
         
         return AuthenticationResponse.builder()
             .token(jwtToken)
             .build();
+    }
+
+    private void revokeAllUserTokens(AppUser user) {
+        var validTokens = tokenRepository.findAllValidTokensByUser(user.getId());
+        if (validTokens.isEmpty()) {
+            return;
+        }
+        validTokens.forEach(t -> {
+            t.setExpired(true);
+            t.setRevoked(true);
+        });
+        tokenRepository.saveAll(validTokens);
+    }
+
+    private void saveUserToken(AppUser user, String jwtToken) {
+        var token = Token.builder()
+            .user(user)
+            .token(jwtToken)
+            .type(TokenType.BEARER)
+            .expired(false)
+            .revoked(false)
+            .build();
+        tokenRepository.save(token);
     }
 
 }
--- security/src/main/java/com/martel/security/demo/DemoController.java
+++ security/src/main/java/com/martel/security/demo/DemoController.java
@@ -1,17 +1,29 @@
 package com.martel.security.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.martel.security.token.Token;
+import com.martel.security.token.TokenRepository;
+
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
-    
 
+    @Autowired
+    private TokenRepository tokenRepository;
+    
     @GetMapping
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("Hello World");
+    }
+    @GetMapping("/tokens")
+    public ResponseEntity<List<Token>> getAllTokens() {
+        return ResponseEntity.ok(tokenRepository.findAll());
     }
 }
